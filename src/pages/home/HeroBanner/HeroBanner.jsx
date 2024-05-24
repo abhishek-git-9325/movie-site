5e40bcc7958aadd79215c0eb3b0f403dc3638dfe
abhishek-git-9325 <abhishek.mourya@amuratech>
2024-05-25 01:59:11 +0530

    Fixed banner background issue

--- src/pages/home/HeroBanner/HeroBanner.jsx
+++ src/pages/home/HeroBanner/HeroBanner.jsx
@@ -1,57 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import "./style.scss"
-import { useNavigate } from 'react-router-dom';
-import useFetch from '../../../hooks/useFetch';
-import { useSelector } from 'react-redux';
-import Img from '../../../components/lazyLoadImage/Img';
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import React, { useEffect, useState } from "react";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
-  const {url} = useSelector(state => state.home);
-  const {data, loading} = useFetch('/movie/upcoming');
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
   // console.log(url);
+  const imgURL = "https://image.tmdb.org/t/p/original";
 
   useEffect(() => {
-    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)].backdrop_path;
+    const bg = url.backdrop
+      ? url.backdrop +
+        data?.results?.[Math.floor(Math.random() * 20)].backdrop_path
+      : imgURL + data?.results?.[Math.floor(Math.random() * 20)].backdrop_path;
     setBackground(bg);
-  }, [data])
+  }, [data]);
   // console.log(background);
-  
 
   const searchHandler = (e) => {
-    if(e.key === 'Enter' && query.length > 0){
+    if (e.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
   };
   return (
     <div className="heroBanner">
-      {!loading && <div className="backdrop-img">
-        <Img src={background} />
-      </div>}
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
       <div className="opacity-layer"></div>
       <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
-              Millions of movies, TV shows and people to discover.
-              Explore now.
+            Millions of movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
-              <input
-                  type="text"
-                  placeholder="Search for a movie or tv show...."
-                  onChange={(e)=> setQuery(e.target.value)}
-                  onKeyUp={searchHandler}
-              />
-              <button>Search</button>
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchHandler}
+            />
+            <button>Search</button>
           </div>
         </div>
       </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
